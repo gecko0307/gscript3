6739172c7e3fdf3149f9cbdeb97579b707f112e4
Timur Gafarov <gecko0307@gmail.com>
2025-09-21 18:37:04 +0300

    Add package import

---
+++ src/gscript/package.d
@@ -0,0 +1,14 @@
+module gscript;
+
+public
+{
+    import gscript.assembler;
+    import gscript.codegen;
+    import gscript.compiler;
+    import gscript.dynamic;
+    import gscript.instruction_set;
+    import gscript.lexer;
+    import gscript.parser;
+    import gscript.serializer;
+    import gscript.vm;
+}
